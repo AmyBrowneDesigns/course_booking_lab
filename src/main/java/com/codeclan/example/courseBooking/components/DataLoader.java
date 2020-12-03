8998d00e448b028dbfc58aeff49835333ac0b794
Amy Browne <amybrowne@users-MacBook-Air.local>
2020-12-03 10:47:01 +0000

    set up models

---
+++ src/main/java/com/codeclan/example/courseBooking/components/DataLoader.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.courseBooking.components;
+
+public class DataLoader {
+}
---
+++ src/main/java/com/codeclan/example/courseBooking/repositories/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.courseBooking.repositories;
+
+public interface CustomerRepository {
+}
