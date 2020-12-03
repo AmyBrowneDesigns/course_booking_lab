343692d016e9eb80bc200039277f239607665a91
Amy Browne <amybrowne@users-MacBook-Air.local>
2020-12-03 11:03:09 +0000

    filled out repos and datalaoder for data

--- src/main/java/com/codeclan/example/courseBooking/components/DataLoader.java
+++ src/main/java/com/codeclan/example/courseBooking/components/DataLoader.java
@@ -1,4 +1,56 @@
 package com.codeclan.example.courseBooking.components;
 
-public class DataLoader {
+import com.codeclan.example.courseBooking.models.Booking;
+import com.codeclan.example.courseBooking.models.Course;
+import com.codeclan.example.courseBooking.models.Customer;
+import com.codeclan.example.courseBooking.repositories.BookingRepository;
+import com.codeclan.example.courseBooking.repositories.CourseRepository;
+import com.codeclan.example.courseBooking.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+
+    @Autowired
+    CourseRepository courseRepository;
+    @Autowired
+    BookingRepository bookingRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public DataLoader(){}
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        //Course
+
+        Course course1 = new Course("Dark spells", "HogsMeade", 3);
+        courseRepository.save(course1);
+        Course course2 = new Course("Herbology", "Greenhouses", 4);
+        courseRepository.save(course2);
+
+        //Customer
+
+        Customer customer1 = new Customer("Harry Potter", "London", 13);
+        customerRepository.save(customer1);
+        Customer customer2 = new Customer("Hermione", "Kent", 14);
+        customerRepository.save(customer2);
+
+
+        //Booking
+
+        Booking booking1 = new Booking("25-12-20", customer1, course2);
+        bookingRepository.save(booking1);
+        Booking booking2 = new Booking("12-01-21", customer2, course1);
+        bookingRepository.save(booking2);
+
+    }
+
+
 }
--- src/main/java/com/codeclan/example/courseBooking/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/example/courseBooking/repositories/BookingRepository.java
@@ -1,4 +1,7 @@
 package com.codeclan.example.courseBooking.repositories;
 
-public interface BookingRepository {
+import com.codeclan.example.courseBooking.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
