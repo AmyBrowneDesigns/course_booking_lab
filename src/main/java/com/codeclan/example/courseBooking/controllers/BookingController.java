27ce67bf5319af47af05afa658c185b6d2c862ac
AvianLearning <iaradcliffe@hotmail.com>
2020-12-03 13:14:58 +0000

    Added get all routes to controllers

--- src/main/java/com/codeclan/example/courseBooking/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/courseBooking/controllers/BookingController.java
@@ -1,4 +1,25 @@
 package com.codeclan.example.courseBooking.controllers;
 
+import com.codeclan.example.courseBooking.models.Booking;
+import com.codeclan.example.courseBooking.models.Course;
+import com.codeclan.example.courseBooking.repositories.BookingRepository;
+import com.codeclan.example.courseBooking.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/bookings")
+    public ResponseEntity<List<Booking>> getAllBookings() {
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/codeclan/example/courseBooking/controllers/CustomerControlller.java
+++ src/main/java/com/codeclan/example/courseBooking/controllers/CustomerControlller.java
@@ -1,4 +1,22 @@
 package com.codeclan.example.courseBooking.controllers;
 
+import com.codeclan.example.courseBooking.models.Customer;
+import com.codeclan.example.courseBooking.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CustomerControlller {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value ="/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
 }
